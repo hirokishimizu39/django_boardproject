c66071bfddb22af35114ba1147e771c581ed5c5b
hiroki shimizu <hiroki71027923@icloud.com>
2024-11-17 11:09:45 +0900

    login機能の実装

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.db import IntegrityError
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def signupfunc(request):
@@ -13,5 +14,18 @@ def signupfunc(request):
       return render(request, 'signup.html', {'some': 100})
     except IntegrityError:
       print('ここまできてる？')
-      return render(request, 'signup.html', {'error': 'このユーザーはすでに存在しています'})
-  return render(request, 'signup.html', {'error': 100})
+      return render(request, 'signup.html', {'error': 'このユーザーはすでに存在しています'})    
+  return render(request, 'signup.html')
+
+
+def loginfunc(request):
+  if request.method == "POST":
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+      login(request, user)
+      return render(request, 'login.html', {'context': 'logged in'})
+    else:
+      return render(request, 'login.html', {'context': 'not logged in'})
+  return render(request, 'login.html', {'context': 'get method'})
